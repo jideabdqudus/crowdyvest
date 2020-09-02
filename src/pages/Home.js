99c77d9e05d8ab92c90460ca230e4c5e4ac239e4
jideabdqudus <jideabdqudus@gmail.com>
2020-09-02 17:11:37 +0100

    Added Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Row, Col } from "antd";
+import { Row, Col, Card } from "antd";
 import HeroTop from "../components/HeroTop";
 import "./pages.css";
 const Home = () => {
@@ -7,10 +7,14 @@ const Home = () => {
     <div>
       <HeroTop />
       <Row>
-        <Col  offset={6} xs={12} sm={12} md={12} lg={12} xl={12}>
+        <Col className="boxCenter" xs={24} sm={12} md={12} lg={12} xl={12}>
           <p className="headingTitle">
-            The Smarter Way to 
-            <br/>Get your Money<br/> Working
+            The Smarter Way to
+            <div>Get your Money</div> Working
+          </p>
+          <p className="headingSubTitle">
+            Crowdyvest connects you to impact-driven 
+            <div>opportunities with good returns</div>.
           </p>
         </Col>
       </Row>
