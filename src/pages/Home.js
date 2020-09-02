f81490e431f556523b3f481a487201e5095785f5
jideabdqudus <jideabdqudus@gmail.com>
2020-09-02 17:40:37 +0100

    Added Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { Row, Col, Card } from "antd";
+import { Row, Col, Card, Divider } from "antd";
 import HeroTop from "../components/HeroTop";
 import "./pages.css";
+import Savings from "../assets/savings.svg";
+import Sponsorships from "../assets/sponsorships.svg";
+import {ArrowRightOutlined} from "@ant-design/icons"
+
+
 const Home = () => {
+  const { Meta } = Card;
   return (
     <div>
       <HeroTop />
@@ -13,9 +19,44 @@ const Home = () => {
             <div>Get your Money</div> Working
           </p>
           <p className="headingSubTitle">
-            Crowdyvest connects you to impact-driven 
+            Crowdyvest connects you to impact-driven
             <div>opportunities with good returns</div>.
           </p>
+        </Col>
+      </Row>
+      <Row gutter={16}>
+        <Col span={6} xl={6}>
+          <Card
+            hoverable={false}
+            cover={
+              <img
+                src={Savings}
+                alt="savings"
+                style={{
+                  width: "150px",
+                  margin: "5px",
+                  padding: "15px",
+                  paddingBottom: "0px",
+                  marginBottom: "0px",
+                }}
+              />
+            }
+          >
+            <Divider />
+            <Meta description="Acheive your financial goals by saving with us daily, weekly, and monthly or lock in your funds and enjoy the most impressive rates on your savings." />
+            <p style={{ marginTop: "10px" }}>
+              Start Saving <ArrowRightOutlined/>
+            </p>
+          </Card>
+        </Col>
+        <Col span={6} xl={6}>
+          <Card title="Card title">Card content</Card>
+        </Col>
+        <Col span={6} xl={6}>
+          <Card title="Card title">Card content</Card>
+        </Col>
+        <Col span={6} xl={6}>
+          <Card title="Card title">Card content</Card>
         </Col>
       </Row>
     </div>
