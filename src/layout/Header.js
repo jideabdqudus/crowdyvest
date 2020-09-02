b989dff62fd69ecaf1b49b4dc6bb72b8a0e9ab32
jideabdqudus <jideabdqudus@gmail.com>
2020-09-02 15:32:12 +0100

    Added Commit

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,15 +1,8 @@
 import React, { useState } from "react";
 
 import { Menu } from "antd";
-import {
-  MailOutlined,
-  AppstoreOutlined,
-  SettingOutlined,
-} from "@ant-design/icons";
 
 import Logo from "../assets/crowdyvest-logo.svg";
-
-const { SubMenu } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("mail");
@@ -23,13 +16,19 @@ const Header = () => {
     <div>
       <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
         <Menu.Item key="logo">
-          <img src={Logo} height="30" />
+          <img src={Logo} alt="Logo" height="30" />
         </Menu.Item>
         <Menu.Item key="home">Home</Menu.Item>
         <Menu.Item key="save">Save</Menu.Item>
         <Menu.Item key="sponsor">Sponsor</Menu.Item>
         <Menu.Item key="faq">FAQs</Menu.Item>
         <Menu.Item key="sign">Sign In</Menu.Item>
+        <Menu.Item
+          key="create"
+          style={{ fontSize: "16px", fontWeight: "bold" }}
+        >
+          Create a free account
+        </Menu.Item>
       </Menu>
     </div>
   );
