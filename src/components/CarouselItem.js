e0fc16b1058c046badabb8047e6de9730eb25bae
jideabdqudus <jideabdqudus@gmail.com>
2020-09-03 11:37:34 +0100

    Added Commit

--- src/components/CarouselItem.js
+++ src/components/CarouselItem.js
@@ -1,31 +1,63 @@
-import React from "react";
-import TextTransition, { presets } from "react-text-transition";
+import React, { Component } from "react";
+import ReactTextTransition, { presets } from "react-text-transition";
+import { Row, Col } from "antd";
 
-const TEXTS = ["Forest", "Building", "Tree", "Color"];
+const randomNumber = () => Math.floor(Math.random() * 9999999999 + 10000000000);
 
-const CarouselItem = () => {
-  const [index, setIndex] = React.useState(0);
+const texts = [
+  "Industry; Innovation & Infrastructure",
+  "Sustainable Cities & Communities",
+  "No Poverty",
+  "Zero Hunger",
+  "Good Health and Well-being",
+  "Partnership for the goals",
+];
 
-  React.useEffect(() => {
-    const intervalId = setInterval(
-      () => setIndex((index) => index + 1),
-      3000 // every 3 seconds
+class CarouselItem extends Component {
+  state = {
+    number: randomNumber(),
+    textIndex: 0,
+    textFastIndex: 0,
+    paragraphIndex: 0,
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        number: randomNumber(),
+        textIndex: this.state.textIndex + 1,
+        paragraphIndex: this.state.paragraphIndex + 1,
+      });
+    }, 4000);
+    setInterval(() => {
+      this.setState({
+        textFastIndex: this.state.textFastIndex + 1,
+      });
+    }, 150);
+  }
+
+  render() {
+    return (
+      <div>
+        <p className="sdgHeader">Our Focus on SDGs</p>
+        <section>
+          <section className="sdgSubHeader">
+            <Row>
+              <Col xs={2} sm={4} md={6} lg={8} xl={8}></Col>
+              <Col xs={20} sm={16} md={12} lg={8} xl={8}>
+                <ReactTextTransition
+                  text={texts[this.state.textIndex % texts.length]}
+                  spring={presets.wobble}
+                  delay={300}
+                />
+              </Col>
+              <Col xs={2} sm={4} md={6} lg={8} xl={8}></Col>
+            </Row>
+          </section>
+        </section>
+      </div>
     );
-  });
-
-  return (
-    <div>
-      <p className="sdgHeader">Our Focus on SDGs</p>
-      <h1>
-        <TextTransition
-          text={TEXTS[index % TEXTS.length]}
-          delay={3000}
-          direction="down"
-          springConfig={presets.wobbly}
-        />
-      </h1>
-    </div>
-  );
-};
+  }
+}
 
 export default CarouselItem;
